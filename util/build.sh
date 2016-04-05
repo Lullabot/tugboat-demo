aa0de4551eb22d210aceb586a4e06aa178ab5158
Ben Chavet <ben.chavet@lullabot.com>
2016-04-05 11:36:12 -0500

    Grab database & files

--- util/build.sh
+++ util/build.sh
@@ -69,3 +69,15 @@ if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
 }
 EOF
+
+## Download & extract files
+apt-get update
+apt-get -y install unzip
+rm -rf files
+curl -L "https://www.dropbox.com/s/0wx8b6ef0l9eiuu/files.zip?dl=1&pv=1" > files.zip
+unzip files.zip
+
+## Download & import database
+curl -L "https://www.dropbox.com/s/cqfyu0nsk2vtfff/tugboat-demo.sql?dl=1&pv=1" > ~/tugboat-demo.sql
+mysql -h mysql -u tugboat -ptugboat -e 'drop database tugboat; create database tugboat;'
+cat ~/tugboat-demo.sql | mysql -h mysql -u tugboat-ptugboat tugboat
