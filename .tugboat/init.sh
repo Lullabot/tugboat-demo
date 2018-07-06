f2b40a5a093cf4c70c7a10597f536b5f78c6c77d
Ben Chavet <ben.chavet@lullabot.com>
2018-07-06 11:48:36 -0500

    yaml config

---
+++ .tugboat/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## Link the docroot
+ln -snf "${TUGBOAT_ROOT}/docroot" "${DOCROOT}"
+
+## Install Drush
+composer --no-ansi global require drush/drush:8.0.5
+
+## Set up settings.php
+cp "${DOCROOT}/sites/default/default.settings.php" "${DOCROOT}/sites/default/settings.php"
+cp "${TUGBOAT_ROOT}/.tugboat/tugboat.settings.php" "${DOCROOT}/sites/default/settings.local.php"
---
+++ .tugboat/update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Download & extract files
+rm -rf "${DOCROOT}/sites/default/files"
+wget -O /tmp/files.zip "https://www.dropbox.com/s/v5zydjej6mzsxs9/files3-5.zip?dl=1&pv=1"
+unzip /tmp/files.zip
+mv /tmp/files "${DOCROOT}/sites/defualt/"
+
+## Download & import database
+wget -O /tmp/database.sql "https://www.dropbox.com/s/9omeceyujt53s83/tugboat-demo3-5-2.sql?dl=1&pv=1"
+mysql -h mysql -u tugboat -ptugboat -e 'drop database tugboat; create database tugboat;'
+cat /tmp/database.sql | mysql -h mysql -u tugboat -ptugboat tugboat
+
+## Cleanup
+rm /tmp/*
