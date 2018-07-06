36a9e8ff63ae04a2080ae997e92e16e3c67afc00
Ben Chavet <ben.chavet@lullabot.com>
2018-07-06 12:00:37 -0500

    yaml config

--- .tugboat/update.sh
+++ .tugboat/update.sh
@@ -4,7 +4,7 @@
 rm -rf "${DOCROOT}/sites/default/files"
 wget -O /tmp/files.zip "https://www.dropbox.com/s/v5zydjej6mzsxs9/files3-5.zip?dl=1&pv=1"
 unzip /tmp/files.zip
-mv /tmp/files "${DOCROOT}/sites/defualt/"
+mv files "${DOCROOT}/sites/defualt/"
 rm /tmp/files.zip
 
 ## Download & import database
