3005b2485ff14de42c5f4f3fa03cbde7c429cfe0
Ben Chavet <ben.chavet@lullabot.com>
2018-07-06 15:20:56 -0500

    yaml config

--- .tugboat/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Download & extract files
-rm -rf "${DOCROOT}/sites/default/files"
-wget -O /tmp/files.zip "https://www.dropbox.com/s/v5zydjej6mzsxs9/files3-5.zip?dl=1&pv=1"
-unzip /tmp/files.zip
-mv files "${DOCROOT}/sites/default/"
-rm /tmp/files.zip
