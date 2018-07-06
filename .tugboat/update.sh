8ff074f13fcaca5b86346f5da5381738998e4569
Ben Chavet <ben.chavet@lullabot.com>
2018-07-06 12:04:17 -0500

    yaml config

--- .tugboat/update.sh
+++ .tugboat/update.sh
@@ -6,9 +6,3 @@ wget -O /tmp/files.zip "https://www.dropbox.com/s/v5zydjej6mzsxs9/files3-5.zip?d
 unzip /tmp/files.zip
 mv files "${DOCROOT}/sites/defualt/"
 rm /tmp/files.zip
-
-## Download & import database
-wget -O /tmp/database.sql "https://www.dropbox.com/s/9omeceyujt53s83/tugboat-demo3-5-2.sql?dl=1&pv=1"
-mysql -h mysql -u tugboat -ptugboat -e 'drop database tugboat; create database tugboat;'
-cat /tmp/database.sql | mysql -h mysql -u tugboat -ptugboat tugboat
-rm /tmp/database.sql
